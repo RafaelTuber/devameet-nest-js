0663d9c66514a3190fa15ba7bf98721ee148e28d
RafaelTuber <RafaelTuber@hotmail.com>
2023-09-17 22:34:18 -0300

    criado o serviço de registro e configurado banco de dados mongoose

---
+++ src/user/helpers/messages.helper.ts
@@ -0,0 +1,7 @@
+export const UserMessagesHelper = {
+    USER_LOGIN_OR_EMAIL_NOT_FOUND : 'E-mail e/ou senha inválidos',
+    REGISTER_NAME_NOT_VALID: 'Nome informado não é válido',
+    REGISTER_EMAIL_NOT_VALID: 'E-mail informado não é válido',
+    REGISTER_PASSWORD_NOT_VALID: 'Senha informada não é válida, ela precisa ter de 4 a 12 caracteres e conter pelo menos um caracter maiusculo, um minúsculo, um numero e um caractér especial.',
+    REGISTER_EXIST_EMAIL_ACCOUNT: 'Já existe uma conta com o email informado.',
+}
