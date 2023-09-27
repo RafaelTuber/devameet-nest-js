e29e46640f4c91dcb118fa6b1288782102af4445
RafaelTuber <RafaelTuber@hotmail.com>
2023-09-26 23:59:49 -0300

    criado login e vinculado ao mongodb . criado a strategy de JWT com validate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,39 @@
-import {BadRequestException, Injectable, Logger} from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { LoginDto } from "./dtos/login.dto";
 import { MessagesHelper } from "./helpers/messages.helper";
 import { RegisterDto } from 'src/user/dtos/register.dto';
 import { UserService } from 'src/user/user.service';
 import { UserMessagesHelper } from 'src/user/helpers/messages.helper';
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
     private logger = new Logger(AuthService.name);
 
-    constructor(private readonly userService: UserService){}
-    login(dto: LoginDto) {
+    constructor(
+        private readonly userService: UserService,
+        private readonly JwtService: JwtService
+    ) { }
+    async login(dto: LoginDto) {
         this.logger.debug('login - started');
-        if (dto.login !== 'teste@teste.com' || dto.password !== 'teste@123') {
+
+        const user = await this.userService.getUserByLoginPassword(dto.login, dto.password)
+        if (user == null) {
             throw new BadRequestException(MessagesHelper.AUTH_PASSWORD_OR_LOGIN_NOT_FOUND);
+        }
+
+        const tokenPayload = { email: user.email, sub: user._id };
+        return {
+            email: user.email,
+            name: user.name,
+            token: this.JwtService.sign(tokenPayload, { secret: process.env.USER_JWT_SECRET_KEY })
         }
 
         return dto;
     }
-    async register(dto: RegisterDto){
+    async register(dto: RegisterDto) {
         this.logger.debug('Register - started');
-        if(await this.userService.existsByEmail(dto.email)){
+        if (await this.userService.existsByEmail(dto.email)) {
             throw new BadRequestException(UserMessagesHelper.REGISTER_EXIST_EMAIL_ACCOUNT);
         }
 
